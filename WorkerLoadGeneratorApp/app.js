3e446691b3f2b10e1f11ab64d0664d826f67c37a
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-24 19:29:23 -0400

    remove http dependency, format location

--- WorkerLoadGeneratorApp/app.js
+++ WorkerLoadGeneratorApp/app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
 
 var app = express();
+var currentapppath= "myapp";
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,8 +19,8 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.text({type: '*/*'}));
-app.use(bodyParser.raw());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,7 +28,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found: '+req.url);
   err.status = 404;
   next(err);
 });
