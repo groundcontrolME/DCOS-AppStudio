92be960bfb6f7c1c412bbbd44b6c392597ffe5c5
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-26 00:52:14 -0400

    first stable commit

--- Actor/actor.py
+++ Actor/actor.py
@@ -15,7 +15,7 @@ import os
 import requests
 import json
 import random
-from faker import Factory	#fake-factory: generates good-looking random data
+from faker import Faker	#fake-factory: generates good-looking random data
 import datetime
 import time 				#required to generate JS-style datetime for msg id
 import math
@@ -30,15 +30,15 @@ WAIT_SECS_SEED = 10			#every random*(10 seconds) we thing of moving
 SUICIDE_CHANCE = 3			#chance of commiting suicide in pct every wait time
 MOVING_CHANCE = 30			#chance of moving in the map
 
-def generate_random_number( min, max, length):
+def generate_random_number( min=0, max=0, length=0 ):
 	"""
 	Generates a random number inside a range or with a specific length
 	"""
-	if length:
+	if length > 0:
 		range_start = 10**( length - 1 )
 		range_end = ( 10**length ) - 1
 		return random.randint( range_start, range_end )
-	else if (min and max):
+	if (min and max):
 		return random.randint( min, max )
 
 def get_random_for_type( my_type ):
@@ -47,18 +47,15 @@ def get_random_for_type( my_type ):
 	Type is a JS type defined by the app, not a python type.
 	"""
 
-	print('**DEBUG: my_type is of type is {0}'.format(type(my_value)))
+	print('**DEBUG: my_type is of type is {0}'.format(my_type))
 
-	return fake.bs() if my_type == "String"
-	return generate_random_number( length=generate_random_number( length=3 ) ) \
-								if my_type == "Integer"
-	return generate_random_number( generate_random_number( length=7 ), \
-	 							if my_type == "Long" )
-	return generate_random_number( min=(-1)*generate_random_number( length=7 ) , \
-								max=*generate_random_number( length=7 ) \
-	 							if my_type == "Double" )
-	return fake.latitude()+","fake.longitude() if type(my_value) == "Location"
-	return fake.dateTime() if ( my_type == "Date/time" or my_type == "Date/Time" )
+	if my_type == "String": return fake.bs() 
+	if my_type == "Integer": return generate_random_number( length=generate_random_number( length=3 ) )		
+	if my_type == "Long": return generate_random_number( generate_random_number( length=7 ) )							
+	if my_type == "Double": return generate_random_number( min=(-1)*generate_random_number( length=7 ) , \
+							max=generate_random_number( length=7 ) )			
+	if my_type == "Location": return str(fake.latitude())+","+str(fake.longitude()) 
+	if ( my_type == "Date/time" or my_type == "Date/Time" ): return str(fake.date_time()) 
 
 	print('**ERROR: my_type is not detected')
 	return None
@@ -68,8 +65,7 @@ def generate_random_location( latitude, longitude, radius ):
 	Generates a random location from lat, long, radius
 	returns in as string in "latitude", "longitude" format.
 	"""
-
-	rd = radius / 111300
+	rd = int(radius) / 111300
 
 	u = float(generate_random_number(length=7))
 	v = float(generate_random_number(length=7))
@@ -79,15 +75,15 @@ def generate_random_location( latitude, longitude, radius ):
 	x = w*math.cos(t)
 	y = w*math.sin(t)
 
-	new_location = str(y+y0)+","str(x+x0)
-	print('**DEBUG: new_location is {0}'.print(new_location))
+	new_location = str(y+float(latitude))+","+str(x+float(longitude))
+	print('**DEBUG: new_location is {0}'.format(new_location))
 
 	return new_location
 
 if __name__ == "__main__":
 
 	#initialize fake data factory
-	fake = Factory.create()
+	fake = Faker()
 	#initialize actor information
 	actor = {}
 
@@ -100,34 +96,38 @@ if __name__ == "__main__":
 	print('**DEBUG Listener is: {0}'.format( listener ) )
 
 	# Read fields from env variable APPDEF
-	appdef = json.loads( os.getenv('APPDEF', "{}")  )
+	appdef_env = os.getenv('APPDEF', {} )
+	print('**APPDEF is: {0}'.format( appdef_env ) )
+	appdef_clean = appdef_env.replace( "'", '"' )[:-1]
+	print('**APPDEF clean is: {0}'.format( appdef_clean ) )
+	appdef = json.loads(appdef_clean)
 	fields = appdef['fields']
 
 	#Generate my location from lat long radius
-	actor['location'] = generate_random_location( latitude, longitude, radius )
+	actor["location"] = generate_random_location( latitude, longitude, radius )
 
 	# Generate my ID - 13 figures number
 	my_id = generate_random_number( length=MY_ID_LENGTH )
 	print('**DEBUG my id is: {0}'.format( my_id ) )
-	actor['uuid'] = my_id
+	actor["uuid"] = my_id
 
 	#loop through the fields, populate
 	for field in fields:
 		#search for "name", if present generate one
 		if field['name'] == "name":
-			actor['name'] = fake.name()
+			actor["name"] = fake.name()
 			print('**DEBUG my name is: {0}'.format( my_age ) )
 			continue
 
 		#search for "age", if present generate age in range that makes sense
 		if field['name'] == "age":
-			actor['age'] = generate_random_number( min=AGE_MIN, max=AGE_MAX )
+			actor["age"] = generate_random_number( min=AGE_MIN, max=AGE_MAX )
 			print('**DEBUG my age is: {0}'.format( my_age ) )
 			continue
 
 		#search for "country", if present generate a country name
 		if field['name'] == "country":
-			actor['country'] = fake.country()
+			actor["country"] = fake.country()
 			print('**DEBUG my country is: {0}'.format( my_country ) )
 			continue
 
@@ -141,13 +141,14 @@ if __name__ == "__main__":
 	# creator API endpoint
 
 	#set my creation (birth) time as now
-	actor['start_time'] = datetime.datetime.now()
+	actor["start_time"] = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y")
 
 	#### All fields are now ready, start posting
-	while true:
+	while True:
 
 		#fill in the message Id with "now" in javascript format
 		actor['id'] = int(time.time() * 1000)
+		print('**DEBUG ACTOR is: {0}'.format( actor ) )
 		#build the request
 		headers = {
 		'Content-type': 'application/json'
@@ -157,10 +158,10 @@ if __name__ == "__main__":
 			request = requests.post(
 				listener,
 				data = json.dumps( actor ),
-				headers = jeaders
+				headers = headers
 				)
-				request.raise_for_status()
-				print("** INFO: sent update: \n{0}".format(actor))
+			request.raise_for_status()
+			print("** INFO: sent update: \n{0}".format(actor))
 		except (
 		    requests.exceptions.ConnectionError ,\
 		    requests.exceptions.Timeout ,\
