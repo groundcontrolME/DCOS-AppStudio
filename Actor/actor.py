b686e339df61b41c6f6520ac7ce920ac97c1a7bf
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-26 12:16:05 -0400

    correct message formatting, add VIP on :0

--- Actor/actor.py
+++ Actor/actor.py
@@ -30,7 +30,7 @@ import math
 DEFAULT_LATITUDE = 41.411338
 DEFAULT_LONGITUDE = 2.226438
 DEFAULT_RADIUS = 300
-MY_ID_LENGTH = 13
+MY_ID_LENGTH = 6			#up to 1 million users - integer
 AGE_MAX = 60
 AGE_MIN = 16
 WAIT_SECS_SEED = 10			#every random*(10 seconds) we thing of moving
@@ -48,21 +48,32 @@ def generate_random_number( min=0, max=0, length=0 ):
 	if (min and max):
 		return random.randint( min, max )
 
-def get_random_for_type( my_type ):
+def get_random_for_type( field ):
 	"""
-	Generates a random value for the type received.
+	Generates a random value for the field received.
+	field is {"name": name, "type": JStype, "pivot": boolean}
 	Type is a JS type defined by the app, not a python type.
 	"""
 
-	print('**DEBUG: my_type is of type is {0}'.format(my_type))
+	my_type = field['type']
+	my_name = field['name']
+
+	print('**DEBUG: Random value for {0} of type {1}'.format(my_name, my_type))
 
 	if my_type == "String": return fake.bs() 
-	if my_type == "Integer": return generate_random_number( length=generate_random_number( length=3 ) )		
-	if my_type == "Long": return generate_random_number( generate_random_number( length=7 ) )							
+	if my_type == "Integer": return generate_random_number( length=2 )		
+	if my_type == "Long": return generate_random_number( length=5 )
 	if my_type == "Double": return generate_random_number( min=(-1)*generate_random_number( length=7 ) , \
 							max=generate_random_number( length=7 ) )			
 	if my_type == "Location": return str(fake.latitude())+","+str(fake.longitude()) 
-	if ( my_type == "Date/time" or my_type == "Date/Time" ): return str(fake.date_time()) 
+	if ( my_type == "Date/time" or my_type == "Date/Time" ): 
+		date = fake.iso8601() #fake date -- ANY date
+		#date = datetime.datetime.now().isoformat()
+		print("**DEBUG: fake date is: {0}".format(date))
+		return(date)
+		#unixdate = time.mktime(date.timetuple())
+		#print("**DEBUG: fake Unixdate is: {0}".format(unixdate))
+		#return str(unixdate) 
 
 	print('**ERROR: my_type is not detected')
 	return None
@@ -92,6 +103,7 @@ if __name__ == "__main__":
 	#initialize fake data factory
 	fake = Faker()
 	#initialize actor information
+	#TODO: check in Cassandra whether I exist? relaunch if I do?
 	actor = {}
 
 	# Parse environment variables
@@ -99,7 +111,7 @@ if __name__ == "__main__":
 	longitude = os.getenv('LONGITUDE', DEFAULT_LONGITUDE)
 	radius = os.getenv('RADIUS', DEFAULT_RADIUS)
 	#Listener POST endpoint is an environment variable
-	listener = os.getenv('LISTENER')
+	listener = os.getenv('LISTENER')+":0"				#all VIPs are created :0
 	print('**DEBUG Listener is: {0}'.format( listener ) )
 
 	# Read fields from env variable APPDEF
@@ -114,12 +126,28 @@ if __name__ == "__main__":
 	actor["location"] = generate_random_location( latitude, longitude, radius )
 
 	# Generate my ID - 13 figures number
-	my_id = generate_random_number( length=MY_ID_LENGTH )
+	# my_id = generate_random_number( length=MY_ID_LENGTH )
+	# initially use 'uuid' as per fixed schema
+	my_id = datetime.datetime.now().isoformat()
 	print('**DEBUG my id is: {0}'.format( my_id ) )
 	actor["uuid"] = my_id
 
 	#loop through the fields, populate
 	for field in fields:
+
+		#search for "id", skip as it's internal from the app/ingesters
+		if field['name'] == "id":
+			continue
+
+		#search for "uuid", skip as we want a single inmutable UUID per user
+		if field['name'] == "uuid":
+			continue
+
+		#search for "event_timestamp", generate manually
+		if field['name'] == "event_timestamp":
+			actor["event_timestamp"] = datetime.datetime.now().isoformat()
+			continue
+
 		#search for "name", if present generate one
 		if field['name'] == "name":
 			actor["name"] = fake.name()
@@ -138,8 +166,11 @@ if __name__ == "__main__":
 			print('**DEBUG my country is: {0}'.format( actor["country"] ) )
 			continue
 
-		#field is not well-known, fill it with gibberish
-		actor[field['name']] = get_random_for_type( field['type'] )
+
+		#field is LEARNED from APPDEF, fill it with gibberish
+		actor[field['name']] = get_random_for_type( field )
+		print('**DEBUG LEARNED field: {0} | generated value: {1}'.format( field['name'], actor[field['name']] ) )
+
 
 	# NOT USED:
 	# Kafka topic
@@ -148,13 +179,16 @@ if __name__ == "__main__":
 	# creator API endpoint
 
 	#set my creation (birth) time as now
-	actor["start_time"] = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y")
+	#actor["start_time"] = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y") '03:44PM April 26, 2017'
 
 	#### All fields are now ready, start posting
 	while True:
 
 		#fill in the message Id with "now" in javascript format
-		actor['id'] = int(time.time() * 1000)
+		#
+		#actor['id'] = int(time.time() * 1000)
+		#fill in the event_timestamp with "now" in javascript format
+		actor['event_timestamp'] = int(time.time() * 1000)
 		print('**DEBUG ACTOR is: {0}'.format( actor ) )
 		#build the request
 		headers = {
