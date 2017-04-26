228a391581e056dc5b056f87cb46d3e47b009666
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-26 15:45:26 -0400

    stable

--- Actor/actor.py
+++ Actor/actor.py
@@ -34,8 +34,8 @@ MY_ID_LENGTH = 6			#up to 1 million users - integer
 AGE_MAX = 60
 AGE_MIN = 16
 WAIT_SECS_SEED = 20			#every random*(10 seconds) we thing of moving
-SUICIDE_CHANCE = 1			#chance of commiting suicide in pct every wait time
-MOVING_CHANCE = 10			#chance of moving in the map
+SUICIDE_CHANCE = 10			#chance of commiting suicide in pct every wait time
+MOVING_CHANCE = 33			#chance of moving in the map
 
 def generate_random_number( min=0, max=0, length=0 ):
 	"""
@@ -100,9 +100,11 @@ def generate_random_location( latitude, longitude, radius ):
 
 def calculate_distance (src_coords, dst_coords):
 	"""
-	Calculates the distance in meters between two coordinates.
-	These are received as a string with "lat,long".
+	#Calculates the distance in meters between two coordinates.
+	#These are received as a string with "lat,long".
 	"""
+
+	R = 6373.0	#earth radius in km
 
 	lat1, lon1 = src_coords.split(',')
 	print("**DEBUG: find src coords: lat is {0} long is {1}".format(lat1, lon1))
@@ -110,14 +112,18 @@ def calculate_distance (src_coords, dst_coords):
 	print("**DEBUG: find dst coords: lat is {0} long is {1}".format(lat1, lon1))
 
 	# convert decimal degrees to radians 
-	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+	#lon1, lat1, lon2, lat2 = map(math.radians, [float(lon1), float(lat1), float(lon2), float(lat2)])
+	lat1 = math.radians(lat1)
+	lon1 = math.radians(lon1)
+	lat2 = math.radians(lat2)
+	lon2 = math.radians(lon2)
 	# haversine formula 
 	dlon = lon2 - lon1 
 	dlat = lat2 - lat1 
-	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-	c = 2 * asin(sqrt(a)) 
-	distance_meters = 6367 * c * 1000   
-
+	a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+	c = 2 * math.asin(math.sqrt(a)) 
+	distance_meters = R * c * 1000   
+	print("**DEBUG: distance METERS is {0}".format(distance_meters))
 	return distance_meters
 
 if __name__ == "__main__":
@@ -139,11 +145,14 @@ if __name__ == "__main__":
 	# Read fields from env variable APPDEF
 	appdef_env = os.getenv('APPDEF', {} )
 	print('**APPDEF is: {0}'.format( appdef_env ) )
-	appdef_clean = appdef_env.replace( "'", '"' )	#[:-1] has happened that the last char is rubbish
-	print('**APPDEF clean is: {0}'.format( appdef_clean ) )
-	appdef = json.loads(appdef_clean)
-	fields = appdef['fields']
-
+	if appdef_env:
+		appdef_clean = appdef_env.replace( "'", '"' )	#[:-1] has happened that the last char is rubbish
+		print('**APPDEF clean is: {0}'.format( appdef_clean ) )
+		appdef = json.loads(appdef_clean)
+		fields = appdef['fields']
+	else:
+		appdef_clean = ""
+		fields = []
 	#Generate my location from lat long radius
 	actor["location"] = generate_random_location( latitude, longitude, radius )
 
@@ -190,6 +199,10 @@ if __name__ == "__main__":
 			actor["country"] = fake.country()
 			print('**DEBUG my country is: {0}'.format( actor["country"] ) )
 			continue
+
+		#search for "route_length", if present ignore it as I'll calculate my length
+		if field['name'] == "route_length":
+			continue		
 
 
 		#field is LEARNED from APPDEF, fill it with gibberish
@@ -247,7 +260,9 @@ if __name__ == "__main__":
 
 		#wait approximate interval of change (randomized)
 		#wait somewhere between 0 and 90 seconds
-		wait_interval = WAIT_SECS_SEED*generate_random_number( length=1 ) 
+		wait_interval = WAIT_SECS_SEED*generate_random_number( length=1 )
+		print("** I'm going to wait here for {0} seconds.".format(wait_interval))
+		time.sleep(int(wait_interval)) 
 
 		#decide randomly whether to change or not (decide how)
 		move_on = ( random.randrange(100) < MOVING_CHANCE )
